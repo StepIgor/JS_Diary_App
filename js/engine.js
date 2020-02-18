f5cb66b9ba0560c03228dfa14b87d6a21c3d935b
StepIgor <igor.stepanov@difres.ru>
2020-02-18 11:52:59 +0300

    now you can add subjects and tasks!

--- js/engine.js
+++ js/engine.js
@@ -1,9 +1,10 @@
 //check first run
 var window_now = "active";
+var last_choice = "";
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('.modal').modal();
-  if (localStorage.getItem('already_used') == null){
+  if (localStorage.getItem('already_used') == null) {
     $("#modal1").modal('open');
 
     var subjects = []
@@ -11,33 +12,38 @@ $(document).ready(function(){
     var arc_subjects = []
     var arc_subj_content = []
 
-    localStorage.setItem("subjects",JSON.stringify(subjects));
-    localStorage.setItem("subj_content",JSON.stringify(subj_content));
-    localStorage.setItem("arc_subjects",JSON.stringify(arc_subjects));
-    localStorage.setItem("arc_subj_content",JSON.stringify(arc_subj_content));
-    localStorage.setItem("already_used","yes");
+    localStorage.setItem("subjects", JSON.stringify(subjects));
+    localStorage.setItem("subj_content", JSON.stringify(subj_content));
+    localStorage.setItem("arc_subjects", JSON.stringify(arc_subjects));
+    localStorage.setItem("arc_subj_content", JSON.stringify(arc_subj_content));
+    localStorage.setItem("already_used", "yes");
   } else {
 
     refresh_subj_list();
 
-}});
+  }
+});
 
-function refresh_subj_list(){
+function refresh_subj_list() {
   var detail = "";
-  if (window_now == "active"){detail = "subjects"} else {detail = "arc_subjects"}
+  if (window_now == "active") {
+    detail = "subjects"
+  } else {
+    detail = "arc_subjects"
+  }
 
   $('#desktop_subject_list').html("");
   $('#mob_subject_list').html("");
-  JSON.parse(localStorage.getItem(detail)).forEach(function(elem){
-    $('#desktop_subject_list').html( $("#desktop_subject_list").html() + "<li><a class=\"waves-effect\" href=\"#!\" onclick=\"show_sj(\'" + elem + "\')\">" + elem + "</a></li>");
-    $('#mob_subject_list').html( $("#mob_subject_list").html() + "<li><a class=\"waves-effect\" href=\"#!\" onclick=\"show_sj(\'" + elem + "\')\">" + elem + "</a></li>");
+  JSON.parse(localStorage.getItem(detail)).forEach(function(elem) {
+    $('#desktop_subject_list').html($("#desktop_subject_list").html() + "<li><a class=\"waves-effect\" href=\"#!\" onclick=\"show_sj(\'" + elem + "\')\">" + elem + "</a></li>");
+    $('#mob_subject_list').html($("#mob_subject_list").html() + "<li><a class=\"waves-effect\" href=\"#!\" onclick=\"show_sj(\'" + elem + "\')\">" + elem + "</a></li>");
   });
 }
 
 
 
-function change_window(win){
-  if (win == "arc"){
+function change_window(win) {
+  if (win == "arc") {
     window_now = "archive";
     $('#mob_section_name').text('Архив');
     $('#desktop_section_name').text('Архив');
@@ -50,21 +56,21 @@ function change_window(win){
   refresh_subj_list();
 }
 
-function addsubject(){
+function addsubject() {
   $('#modal2').modal('open');
   $('#slide-out').sidenav('close');
 }
 
-function add_subject_now(){
+function add_subject_now() {
   var newsubj = $('#new_subj_name').val();
   $('#new_subj_name').val("")
 
-  if (newsubj.length > 0){
+  if (newsubj.length > 0) {
     var subjects = JSON.parse(localStorage.getItem('subjects'));
 
-    if (subjects.indexOf(newsubj) == -1){
+    if (subjects.indexOf(newsubj) == -1) {
       subjects.push(newsubj);
-      localStorage.setItem('subjects',JSON.stringify(subjects));
+      localStorage.setItem('subjects', JSON.stringify(subjects));
       $('#modal2').modal('close');
       refresh_subj_list();
     } else {
@@ -74,23 +80,88 @@ function add_subject_now(){
 }
 
 
-function show_sj(id){
+function show_sj(id) {
+  last_choice = id;
+  $("#sj_tasks_lines").html("");
   $('#sj_name_now').text(id);
   $('#slide-out').sidenav('close');
 
   var sind = -1;
 
   var detail = "";
-  if (window_now == "active"){detail = "subjects_content"} else {detail = "arc_subj_content"}
+  if (window_now == "active") {
+    detail = "subj_content"
+  } else {
+    detail = "arc_subj_content"
+  }
 
-  if (JSON.parse(localStorage.getItem(detail)) != null){
-    JSON.parse(localStorage.getItem(detail)).forEach(function(elem,ind){
-      if (elem[0] == id){
+  if (JSON.parse(localStorage.getItem(detail)) != "") {
+    JSON.parse(localStorage.getItem(detail)).forEach(function(elem, ind) {
+      if (elem[0] == id) {
         sind = ind;
       }
     });
+
+    if (sind != -1) {
+      for (item = 1; item < JSON.parse(localStorage.getItem(detail))[sind].length; item++) {
+        $("#sj_tasks_lines").html($("#sj_tasks_lines").html() + '<a href="#!" class="collection-item black-text">' + JSON.parse(localStorage.getItem(detail))[sind][item][0] + '</a>');
+      }
+    }
+
   }
 
 
-  $('#sj_tasks').css({"display":"block"});
+  $('#sj_tasks').css({
+    "display": "block"
+  });
+}
+
+
+function open_new_task_form() {
+  $('#modal3').modal('open');
+}
+
+
+function add_task_now() {
+  var task_text = $('#new_task_text').val();
+
+  if (task_text.length > 0) {
+
+    var sind = -1;
+    var detail = "";
+    if (window_now == "active") {
+      detail = "subj_content"
+    } else {
+      detail = "arc_subj_content"
+    }
+
+    if (JSON.parse(localStorage.getItem(detail)) != "") {
+      JSON.parse(localStorage.getItem(detail)).forEach(function(elem, ind) {
+        if (elem[0] == last_choice) {
+          sind = ind;
+        }
+      });
+
+
+      if (sind != -1){
+        var lst = JSON.parse(localStorage.getItem(detail));
+        lst[sind].push([task_text,0]);
+        localStorage.setItem(detail,JSON.stringify(lst));
+      } else {
+        var lst = JSON.parse(localStorage.getItem(detail));
+        lst.push([last_choice,[task_text,0]]);
+        localStorage.setItem(detail,JSON.stringify(lst));
+      }
+
+      $('#modal3').modal('close');
+    } else {
+      var lst = [last_choice,[task_text,0]];
+      localStorage.setItem(detail,JSON.stringify(lst));
+    }
+
+    $('#new_task_text').val("");
+
+    show_sj(last_choice);
+
+  }
 }
