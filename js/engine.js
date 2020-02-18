26ac83e913432a69ea6df523489e1d1e9bb8d74f
StepIgor <igor.stepanov@difres.ru>
2020-02-18 10:10:18 +0300

    subj list + nav between archive and active

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,6 @@
 //check first run
+var window_now = "active";
+
 $(document).ready(function(){
   $('.modal').modal();
   if (localStorage.getItem('already_used') == null){
@@ -9,16 +11,30 @@ $(document).ready(function(){
     var arc_subjects = []
     var arc_subj_content = []
 
-    localStorage.setItem("subjects",subjects);
-    localStorage.setItem("subj_content",subj_content);
-    localStorage.setItem("arc_subjects",arc_subjects);
-    localStorage.setItem("arc_subj_content",arc_subj_content);
+    localStorage.setItem("subjects",JSON.stringify(subjects));
+    localStorage.setItem("subj_content",JSON.stringify(subj_content));
+    localStorage.setItem("arc_subjects",JSON.stringify(arc_subjects));
+    localStorage.setItem("arc_subj_content",JSON.stringify(arc_subj_content));
     localStorage.setItem("already_used","yes");
-  }
+  } else {
 
-})
+    refresh_subj_list();
 
-var window_now = "active";
+}});
+
+function refresh_subj_list(){
+  var detail = "";
+  if (window_now == "active"){detail = "subjects"} else {detail = "arc_subjects"}
+
+  $('#desktop_subject_list').html("");
+  $('#mob_subject_list').html("");
+  JSON.parse(localStorage.getItem(detail)).forEach(function(elem){
+    $('#desktop_subject_list').html( $("#desktop_subject_list").html() + "<li><a class=\"waves-effect\" href=\"#!\" onclick=\"show_sj("+elem+")\">" + elem + "</a></li>");
+    $('#mob_subject_list').html( $("#mob_subject_list").html() + "<li><a class=\"waves-effect\" href=\"#!\" onclick=\"show_sj("+elem+")\">" + elem + "</a></li>");
+  });
+}
+
+
 
 function change_window(win){
   if (win == "arc"){
@@ -31,4 +47,28 @@ function change_window(win){
     $('#desktop_section_name').text('Активное')
   }
   $('#slide-out').sidenav('close');
+  refresh_subj_list();
+}
+
+function addsubject(){
+  $('#modal2').modal('open');
+  $('#slide-out').sidenav('close');
+}
+
+function add_subject_now(){
+  var newsubj = $('#new_subj_name').val();
+  $('#new_subj_name').val("")
+
+  if (newsubj.length > 0){
+    var subjects = JSON.parse(localStorage.getItem('subjects'));
+
+    if (subjects.indexOf(newsubj) == -1){
+      subjects.push(newsubj);
+      localStorage.setItem('subjects',JSON.stringify(subjects));
+      $('#modal2').modal('close');
+      refresh_subj_list();
+    } else {
+      $('#add_error').text("Предмет с таким названием уже существует.");
+    }
+  }
 }
